4d55055e525170735c5b45468718eed3a31362b4
Neytan Belisário <neytan.belisario@romancemoda.com.br>
2025-02-05 17:38:42 -0300

    ajustes responsividade

--- src/Components/ToDoList/page.tsx
+++ src/Components/ToDoList/page.tsx
@@ -7,8 +7,8 @@ export const ToDoList = () => {
     return (
         <div className='toDoList'>
             <p style={{textAlign: 'center', fontWeight: '800', fontSize: '26px', color: '#717171'}}>TO DO LIST</p>
-            <Tarefas/>
             <Filtrar/>
+            <Tarefas/>
         </div>
     )
 }
